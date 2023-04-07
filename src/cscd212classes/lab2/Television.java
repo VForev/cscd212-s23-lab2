7f2cabcd701c6872508c81cf5f822ffee7ed0262
VForev <85714161+VForev@users.noreply.github.com>
2023-04-07 00:32:40 -0700

    classes-methods-made

--- src/cscd212classes/lab2/Television.java
+++ src/cscd212classes/lab2/Television.java
@@ -1,4 +1,62 @@
 package cscd212classes.lab2;
 
-public class Television {
+public class Television extends Object implements Comparable<Television> {
+
+    private final boolean fourK;
+
+    private final String make;
+
+    private final String model;
+
+    private final int resolution;
+
+    private final int screenSize;
+
+    private final boolean smart;
+
+    public Television(String make, String model, int resolution, int screenSize, boolean smart) {
+        this.fourK = fourK;
+        this.make = make;
+        this.model = model;
+        this.resolution = resolution;
+        this.screenSize = screenSize;
+        this.smart = smart;
+    }
+
+    public Television(String make, String model, int resolution, int screenSize, boolean smart) {
+        this.fourK = fourK;
+        this.make = make;
+        this.model = model;
+        this.resolution = resolution;
+        this.screenSize = screenSize;
+        this.smart = smart;
+    }
+
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public int getScreenSize() {
+        return screenSize;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Television o) {
+        return 0;
+    }
 }
--- src/cscd212comparators/lab2/TelevisionModelScreenSizeComparator.java
+++ src/cscd212comparators/lab2/TelevisionModelScreenSizeComparator.java
@@ -1,4 +1,14 @@
 package cscd212comparators.lab2;
 
-public class TelevisionModelScreenSizeComparator {
+import cscd212classes.lab2.Television;
+
+import java.util.Comparator;
+
+public class TelevisionModelScreenSizeComparator extends Object implements Comparator<Television> {
+
+    public int compare(Television t1, Television t2){
+        return 0;
+
+    }
+
 }
